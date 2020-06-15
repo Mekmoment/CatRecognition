41f14f6b9df3a5450f30ce5273378e10d4e1bdc0
Phanfa Laohasinnuruk <phanfalaohasinnuruk@Phanfas-MacBook-Pro.local>
2020-06-15 15:40:15 +0700

    Packages needed and Data Manipulation

--- packages.py
+++ packages.py
@@ -5,7 +5,7 @@ import scipy
 from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
-
+"""Packages needed and Data Manipulation"""
 # Loading the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
