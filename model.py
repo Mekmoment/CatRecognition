96f8d1c920c7df9d066e8359463c12c627df4565
Phanfa Laohasinnuruk <phanfalaohasinnuruk@Phanfas-MacBook-Pro.local>
2020-06-15 15:36:48 +0700

    The main function to run the program

--- model.py
+++ model.py
@@ -3,8 +3,9 @@ from predict import predict
 from initialize import initialize
 import numpy as np
 import packages
-def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
 
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    """The main function"""
     # initialize parameters
     w, b = initialize(X_train.shape[0])
 
